57b996fbc1b8a28fcf94f72ad33017ba6781bcd2
austinkelsay <austinkelsay@protonmail.com>
2025-10-21 10:14:11 -0500

    cleaner setup command instructions

--- src/components/share/ShareLoad.tsx
+++ src/components/share/ShareLoad.tsx
@@ -4,13 +4,14 @@ import {ShareNamespaceFrame, ShareInvocationHint} from './ShareNamespaceFrame.js
 
 type ShareLoadProps = {
   args: string[];
+  flags: Record<string, string | boolean>;
   invokedVia?: ShareInvocationHint;
 };
 
-export function ShareLoad({args, invokedVia = 'share'}: ShareLoadProps) {
+export function ShareLoad({args, flags, invokedVia = 'share'}: ShareLoadProps) {
   return (
     <ShareNamespaceFrame invokedVia={invokedVia}>
-      <KeysetLoad args={args} />
+      <KeysetLoad args={args} flags={flags} />
     </ShareNamespaceFrame>
   );
 }
